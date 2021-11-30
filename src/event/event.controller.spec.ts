5edab8174f660e650e72e48c237162621966c1a6
martin-dsp <kcs19542001@hotmail.com>
2021-11-30 21:20:05 +0900

    Feat test code for event controller start

--- src/event/event.controller.spec.ts
+++ src/event/event.controller.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventController } from './event.controller';
+import { EventRepository } from './event.repository';
 import { EventService } from './event.service';
 
 describe('EventController', () => {
   let controller: EventController;
+  let service: EventService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +14,17 @@ describe('EventController', () => {
     }).compile();
 
     controller = module.get<EventController>(EventController);
+    service = module.get<EventService>(EventService);
   });
-
+  // 기본 테스트
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('createEvents', () => {
+    it('should call the service', () => {
+      expect(typeof controller.create).toBe('function');
+      expect(controller.create).toBeCalledWith();
+    });
   });
 });
